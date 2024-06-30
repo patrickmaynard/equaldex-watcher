b48e808c5abfb346bf3f3b413e91a624b44d55aa
patrickmaynard <patrickmaynard.bs@gmail.com>
2024-06-30 23:02:43 +0200

    add settings file

---
+++ settings.php
@@ -0,0 +1,10 @@
+<?php
+
+//Note that the api_key value in this file can be overriden via a .env file.
+//If doing that, just set the value API_KEY in that file.
+
+$settings = [
+    'api_key' => '1a2b3c4d5e6f7',
+    'location_id' => '1a2b3c4d5e6f7',
+    'expected_rank' => 12345
+];
