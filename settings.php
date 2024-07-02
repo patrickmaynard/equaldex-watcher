120c3c20589079a20f7a6baa6be42ede0645a6a3
Patrick Maynard <patrickmaynard.bs@gmail.com>
2024-07-02 20:10:57 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,8 +1,5 @@
 <?php
 
-//Note that the api_key value in this file can be overriden via a .env file.
-//If doing that, just set the value API_KEY in that file.
-
 $settings = [
     'api_key' => '1a2b3c4d5e6f7',
     'recipient_address' => 'john.doe@example.com',
